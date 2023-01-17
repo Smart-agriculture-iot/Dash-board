08e529aa0a13541b2499acecd4c6da3a456a0b65
ndatumuremyi <paternenught@gmail.com>
2023-01-17 23:42:27 +0200

    ft login

--- src/scenes/login/index.js
+++ src/scenes/login/index.js
@@ -1,64 +1,109 @@
 // import AdminScaffold from "../../layouts/AdminScaffold";
 import {Link, useNavigate} from "react-router-dom";
+import joi from 'joi';
+import {joiResolver} from "@hookform/resolvers/joi";
+import swal from "sweetalert";
+import {useForm} from "react-hook-form";
+import axios from "axios";
+
+const fields = {
+    username: joi.string().required(),
+    password: joi.string().required(),
+};
+
+const schema = joi.object(fields);
 const LoginActivity = () => {
+    const navigate = useNavigate();
+    const onSubmit = async (query) => {
+        console.log(query)
+        const formData = new FormData();
+        formData.set("username", query["username"])
+        formData.set("password", query.password);
+
+        axios.post("http://197.243.25.120:5005/login/token", formData).then(response =>{
+            if(response.status === 200){
+                const {data:access_token} = response;
+                localStorage.setItem("access_token", access_token);
+                navigate("/")
+            }
+            console.log(response)
+        }).catch(error => {
+            console.error(error)
+            swal('Login fail!', error.response.data.message || 'incorrect username or password', 'error');
+        })
+    }
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        getValues,
+        reset,
+        formState: { errors },
+    } = useForm({
+        resolver: joiResolver(schema),
+    });
+
     return (
         // <AdminScaffold>
         
-<div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
-  <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+<div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+  <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
   <center><img src="https://i0.wp.com/digitalumuganda.com/wp-content/uploads/2019/09/LOGO-DIGITAL-UMUGANDA.png?w=2597&ssl=1" width="100px" height="100px"></img></center>
-    <h1 class="font-bold text-center text-2xl mb-5">Digital Umuganda </h1>  
-    <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-      <div class="px-5 py-7">
-        <label class="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
-        <input type="email" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
-        <label class="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
-        <input type="password" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
-        <Link to="/home">
-        <button type="button" class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block" >
-            <span class="inline-block mr-2">Login</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-            </svg>
-        </button>
-        </Link>
-      </div>
-      {/* <div class="p-5">
-          <div class="grid grid-cols-3 gap-1">
-              <button type="button" class="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block">MailUp</button>
-              <button type="button" class="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block">Google</button>
-              <button type="button" class="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block">Github</button>
+    <h1 className="font-bold text-center text-2xl mb-5">Digital Umuganda </h1>  
+    <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+      <form className="px-5 py-7" onSubmit={event => {
+          handleSubmit(onSubmit)(event);
+      }}
+      >
+        <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
+        <input {...register("username")}
+            type="email" className="border text-black rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+        <label className="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+        <input {...register("password")}
+            type="password" className="border text-black rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+          <button type="submit" className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block" >
+              <span className="inline-block mr-2">Login</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+              </svg>
+          </button>
+      </form>
+      {/* <div className="p-5">
+          <div className="grid grid-cols-3 gap-1">
+              <button type="button" className="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block">MailUp</button>
+              <button type="button" className="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block">Google</button>
+              <button type="button" className="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block">Github</button>
           </div>
       </div> */}
-        <div class="py-5">
-        <div class="grid grid-cols-2 gap-1">
-          <div class="text-center sm:text-left whitespace-nowrap">
-            <button class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block align-text-top">
+        <div className="py-5">
+        <div className="grid grid-cols-2 gap-1">
+          <div className="text-center sm:text-left whitespace-nowrap">
+            <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block align-text-top">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
                 </svg>
-                <span class="inline-block ml-1">Forgot Password</span>
+                <span className="inline-block ml-1">Forgot Password</span>
             </button>
           </div>
-          <div class="text-center sm:text-right whitespace-nowrap">
-            <button class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block align-text-bottom	">
+          <div className="text-center sm:text-right whitespace-nowrap">
+            <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block align-text-bottom	">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
                 </svg>
-                <span class="inline-block ml-1">Help</span>
+                <span className="inline-block ml-1">Help</span>
             </button>
           </div>
         </div>
       </div>
     </div>
-    <div class="py-5">
-        <div class="grid grid-cols-2 gap-1">
-          <div class="text-center sm:text-left whitespace-nowrap">
-            <button class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-300 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block align-text-top">
+    <div className="py-5">
+        <div className="grid grid-cols-2 gap-1">
+          <div className="text-center sm:text-left whitespace-nowrap">
+            <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-300 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block align-text-top">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                 </svg>
-                <span class="inline-block ml-1">Visit Digitalumuganda.com</span>
+                <span className="inline-block ml-1">Visit Digitalumuganda.com</span>
             </button>
           </div>
         </div>
@@ -87,5 +132,5 @@ export default LoginActivity;
 
 // 	</footer>
 
-// <svg class="absolute bottom-0 left-0 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,0L40,42.7C80,85,160,171,240,197.3C320,224,400,192,480,154.7C560,117,640,75,720,74.7C800,75,880,117,960,154.7C1040,192,1120,224,1200,213.3C1280,203,1360,149,1400,122.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+// <svg className="absolute bottom-0 left-0 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,0L40,42.7C80,85,160,171,240,197.3C320,224,400,192,480,154.7C560,117,640,75,720,74.7C800,75,880,117,960,154.7C1040,192,1120,224,1200,213.3C1280,203,1360,149,1400,122.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
 // <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js"></script>
